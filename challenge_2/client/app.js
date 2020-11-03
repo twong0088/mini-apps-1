6641c52bb16bacc3d30fd5d6043587983d7c44ec
Terrence Wong <terrencewong223@gmail.com>
2020-11-03 15:28:13 -0800

    created form and app.js for data entry

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,23 @@
+var newEntry = {
+  firstName: undefined,
+  lastName: undefined,
+  county: undefined,
+  city: undefined,
+  role: undefined,
+  sales: undefined
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  newEntry.firstName = event.target[0].value;
+  newEntry.lastName = event.target[1].value;
+  newEntry.county = event.target[2].value;
+  newEntry.city = event.target[3].value;
+  newEntry.role = event.target[4].value;
+  newEntry.sales = Number(event.target[5].value);
+
+  document.getElementById("main").reset();
+}
+
 
 
