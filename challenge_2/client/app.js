4ec0a5f25047b2aae7e5ec853a4a9a8292ebff35
Terrence Wong <terrencewong223@gmail.com>
2020-11-03 22:42:21 -0800

    added download csv functionality

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,14 +1,14 @@
-var newEntry = {
-  firstName: undefined,
-  lastName: undefined,
-  county: undefined,
-  city: undefined,
-  role: undefined,
-  sales: undefined
-}
-
 function submitForm(event) {
   event.preventDefault();
+
+  var newEntry = {
+    firstName: undefined,
+    lastName: undefined,
+    county: undefined,
+    city: undefined,
+    role: undefined,
+    sales: undefined
+  }
   newEntry.firstName = event.target[0].value;
   newEntry.lastName = event.target[1].value;
   newEntry.county = event.target[2].value;
@@ -17,8 +17,55 @@ function submitForm(event) {
   newEntry.sales = Number(event.target[5].value);
 
   document.getElementById("main").reset();
+  postNewEntry(newEntry);
+}
+
+function download() {
+  let http = new XMLHttpRequest();
+  http.open('GET', '/users', true);
+
+   // onload function to get data
+   http.onload = function () {
+      if (this.status === 200) {
+          downloadString(this.responseText);
+      }
+   }
+  // Send function to send data
+  http.send()
+
+}
+
+function postNewEntry(newEntry) {
+  let xhr = new XMLHttpRequest();
+   xhr.open('POST',
+   '/newuser', true);
+
+   xhr.setRequestHeader('Content-type', 'application/json');
+
+   xhr.onload = function () {
+       if(this.status === 200){
+           console.log(this.responseText)
+       }
+       else {
+           console.log("Some error occured")
+       }
+   }
+
+   xhr.send(JSON.stringify(newEntry));
 }
 
 
+function downloadString(text, fileType, fileName) {
+  var blob = new Blob([text], { type: fileType });
 
+  var a = document.createElement('a');
+  a.download = fileName;
+  a.href = URL.createObjectURL(blob);
+  a.dataset.downloadurl = [fileType, a.download, a.href].join(':');
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
+}
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
+var string = 'firstName,lastName,county,city,role,sales';
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
+var jsonParser = bodyParser.json();
+app.use(express.static('/Users/caijiaxin/Documents/Hack_Reactor/hrsf131-mini-apps-1/challenge_2/client'));
+app.post('/newuser', jsonParser,(req, res)=> {
+  string += '\n' + `${req.body.firstName},${req.body.lastName},${req.body.county},${req.body.city},${req.body.role},${req.body.sales}`;
+  res.send(string);
+})
+
+app.get('/users', (req, res) => {
+  res.send(string);
+})
 app.listen(port, ()=>{
   console.log(`listening at http://localhost:${port}`);
 })
+
+
+
