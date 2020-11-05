c866f233d2c4601745b7b16e716d3c58cc94c1ea
Terrence Wong <terrencewong223@gmail.com>
2020-11-04 19:38:16 -0800

    established express server

---
+++ challenge_3/server/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('/Users/caijiaxin/Documents/Hack_Reactor/hrsf131-mini-apps-1/challenge_3/client'))
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
